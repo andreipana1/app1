929e3f2fffbb96246fbe680c542c46de3eb67bb9
Davi Pereira <davi.p.m94@gmail.com>
2023-09-25 16:26:56 -0300

    user menu update

--- src/components/navbar/UserMenu.tsx
+++ src/components/navbar/UserMenu.tsx
@@ -13,7 +13,9 @@ export default function UserMenu({ currentUser }: UserMenuProps) {
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
-        <div className="">Airbnb your home</div>
+        <div className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
+          Airbnb your home
+        </div>
       </div>
     </div>
   );
