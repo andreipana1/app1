248f7947c41e03959ef4a6fd2e57c6bcef280cbb
Davi Pereira <davi.p.m94@gmail.com>
2023-09-25 02:47:03 -0300

    adding listing route

---
+++ src/app/api/listings/[listingId] /route.ts
@@ -0,0 +1,33 @@
+import { NextResponse as res } from "next/server";
+
+import prisma from "@/utils/connect";
+import getCurrentUser from "@/actions/getCurrentUser";
+
+interface IParams {
+  params: {
+    listingId?: string;
+  };
+}
+
+export async function DELETE(req: Request, { params }: IParams) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return res.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  const listing = await prisma.listing.deleteMany({
+    where: {
+      id: listingId,
+      userId: currentUser.id,
+    },
+  });
+
+  return res.json(listing, { status: 200 });
+}
---
+++ src/app/api/listings/route.ts
@@ -0,0 +1,49 @@
+import { NextResponse as res } from "next/server";
+
+import prisma from "@/utils/connect";
+import getCurrentUser from "@/actions/getCurrentUser";
+
+export async function POST(req: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return res.error();
+  }
+
+  const body = await req.json();
+
+  const {
+    title,
+    description,
+    imageSrc,
+    category,
+    roomCount,
+    bathroomCount,
+    guestCount,
+    location,
+    price,
+  } = body;
+
+  Object.keys(body).forEach((value) => {
+    if (!body[value]) {
+      res.error();
+    }
+  });
+
+  const listing = await prisma.listing.create({
+    data: {
+      title,
+      description,
+      imageSrc,
+      category,
+      roomCount,
+      bathroomCount,
+      guestCount,
+      locationValue: location.value,
+      price: parseInt(price, 10),
+      userId: currentUser.id,
+    },
+  });
+
+  return res.json(listing, { status: 200 });
+}
