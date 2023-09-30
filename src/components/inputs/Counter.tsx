cf44a572aa46b1f6a7a7fa8eb987d3152229b38e
Davi Pereira <davi.p.m94@gmail.com>
2023-09-30 03:08:32 -0300

    adding some counter

--- src/components/inputs/Counter.tsx
+++ src/components/inputs/Counter.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 interface CounterProps {
   title: string;
@@ -13,5 +14,31 @@ export default function Counter({
   subtitle,
   title,
 }: CounterProps) {
-  return <div>Counter</div>;
+  const onAdd = useCallback(() => {
+    onChange(value + 1);
+  }, [onChange, value]);
+
+  const onReduce = useCallback(() => {
+    if (value === 1) return;
+    onChange(value + 1);
+  }, [onChange, value]);
+
+  return (
+    <div className="counter">
+      <div className="control__header">
+        <div className="font-medium">{title}</div>
+        <div className="font-light text-gray-600">{subtitle}</div>
+      </div>
+
+      <div className="flex flex-row items-center gap-4">
+        <div onClick={onReduce} className="control-counter">
+          <AiOutlineMinus />
+        </div>
+        <div className="font-light text-xl text-neutral-600">{value}</div>
+        <div onClick={onAdd} className="control-counter">
+          <AiOutlinePlus />
+        </div>
+      </div>
+    </div>
+  );
 }
