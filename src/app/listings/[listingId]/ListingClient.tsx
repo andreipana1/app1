34d39c03b9a6b7b7c7c8900494e4166ac8e91b8f
Davi Pereira <davi.p.m94@gmail.com>
2023-10-07 01:43:14 -0300

    adding search modal

---
+++ src/app/listings/[listingId]/ListingClient.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ListingClient() {
+  return (
+    <div>
+      <div>ListingClient</div>
+    </div>
+  );
+}
--- src/app/listings/[listingId]/page.tsx
+++ src/app/listings/[listingId]/page.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import getCurrentUser from "@/actions/getCurrentUser";
+import getListingById from "@/actions/getListingById";
+import getReservations from "@/actions/getReservations";
+import ListingClient from "@/app/listings/[listingId]/ListingClient";
+import EmptyState from "@/components/EmptyState";
 
-export default function ListingsPage() {
-  return (
-    <div>
-      <div>ListingsPage ID</div>
-    </div>
-  );
+type Props = {
+  params: {
+    listingId?: string;
+  };
+};
+export default async function ListingsPage({ params }: Props) {
+  const listing = await getListingById(params);
+  const reservations = await getReservations(params);
+  const currentUser = await getCurrentUser();
+
+  if (!listing) return <EmptyState />;
+
+  return <div>ListingClient</div>;
 }
