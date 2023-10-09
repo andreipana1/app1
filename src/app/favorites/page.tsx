a56484b60245925a563425edd12f75f0b6b271de
Davi Pereira <davi.p.m94@gmail.com>
2023-10-08 21:19:19 -0300

    adding favorites

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import Container from "@/components/Container";
 import EmptyState from "@/components/EmptyState";
 import Heading from "@/components/Heading";
@@ -7,15 +5,15 @@ import ListingCard from "@/components/listings/ListingCard";
 import { getCurrentUser } from "@/utils/auth";
 
 export default async function FavoritePage() {
-  const currentUser = await getCurrentUser();
+  const { user } = await getCurrentUser();
 
-  if (!currentUser) return <EmptyState />;
+  if (!user.favorites?.length) return <EmptyState />;
 
   return (
     <Container>
       <Heading title="Favorites" subtitle="List of places you favorited!" />
       <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
-        {currentUser.user.favorites?.map((favorite) => (
+        {user.favorites?.map((favorite) => (
           <ListingCard key={favorite.id} data={favorite} />
         ))}
       </div>
